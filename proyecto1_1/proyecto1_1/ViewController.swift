e25a82cb4da70eb39f6c654e46fa849f91afe7fc
Daniel Alves <daniel@wedapp.io>
2020-05-10 12:14:15 -0400

    proyecto Xcode creado

---
+++ proyecto1_1/proyecto1_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyecto1_1
+//
+//  Created by Weda Alves on 10-05-20.
+//  Copyright © 2020 wedapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
